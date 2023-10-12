be4073e243ea5cfdac0feaec975ebd65ef054970
Joao <jpmaresp@gmail.com>
2023-10-12 10:39:37 -0300

    getting data dinamically

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,15 +1,27 @@
 export const dataFake = [
   {
-    "id": 1,
-    "picture": "aaa",
-    "title": "ghibli",
-    "description": "dawdawd"
+    "id": "1",
+    "picture": "https://uploads.jovemnerd.com.br/wp-content/uploads/2020/10/imagens-em-alta-studio-ghibli.jpg",
+    "title": "Studio Ghibli disponibiliza mais 300 imagens de A Princesa Mononoke e outros filmes",
+    "description": "Seus novos wallpapers do computador ou do celular"
   },
   {
-    "id": 2,
-    "picture": "aaa",
-    "title": "ghibli",
-    "description": "dawdawd"
+    "id": "2",
+    "picture": "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F566085189%2F1081995196153%2F1%2Foriginal.20230801-122510?w=600&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C0%2C2160%2C1080&s=1856214683e36bc823ba349909246282",
+    "title": "The Art History of Studio Ghibli with Helen McCarthy",
+    "description": "1 de Novembro de 2023"
+  },
+  {
+    "id": "3",
+    "picture": "https://assets.architecturaldigest.in/photos/600822c83a9d9570bc840f08/4:3/w_1024,h_768,c_limit/My-Neighbor-Totoro-Studio-Ghibli-theme-park-in-Japan-1366x768.jpg",
+    "title": "Studio Ghibli reveals fantastical plans for new theme park in Japan",
+    "description": "25 de Maio de 2018"
+  },
+  {
+    "id": "4",
+    "picture": "https://files.tecnoblog.net/wp-content/uploads/2021/05/animacoes-Studio-Ghibli-Netflix-3.jpg",
+    "title": "Os filmes animados do Studio Ghibli em ordem de lançamento",
+    "description": "28 de março de 2021"
   }
 ];
 
